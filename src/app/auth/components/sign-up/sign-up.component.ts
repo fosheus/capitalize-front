fb3d3b13bc693e59f4e0762355b57b2c8451722e
Alban Juteau <juteau.alban@gmail.com>
2021-06-01 13:03:42 +0200

    updated signup form

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/core/services/authentication/authentication.service';
 import { User } from '../../../core/models/user.model';
@@ -12,13 +12,15 @@ import { User } from '../../../core/models/user.model';
 export class SignUpComponent implements OnInit {
 
   signupForm = new FormGroup({
-    firstname: new FormControl(''),
-    lastname: new FormControl(''),
-    email: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    username: new FormControl(''),
-    password: new FormControl('')
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    confirmPassword: new FormControl('', Validators.required),
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
   });
+
+  public errorSignup = false;
 
   constructor(private authService: AuthenticationService, private router: Router) { }
 
@@ -26,18 +28,20 @@ export class SignUpComponent implements OnInit {
   }
 
   onSubmit(): void {
-    if (this.signupForm.controls.password.value === this.signupForm.controls.confirmPassword.value) {
-      const user = new User(
-        this.signupForm.controls.firstname.value,
-        this.signupForm.controls.lastname.value,
-        this.signupForm.controls.email.value,
-        this.signupForm.controls.username.value,
-        this.signupForm.controls.password.value
-      );
-      this.authService.signUp(user).subscribe(() => {
-        this.router.navigateByUrl('/auth/login');
-      });
+    if (!this.signupForm.valid || this.signupForm.controls.password.value !== this.signupForm.controls.confirmPassword.value) {
+      this.errorSignup = true;
+      return;
     }
+    const user = new User(
+      this.signupForm.controls.firstname.value,
+      this.signupForm.controls.lastname.value,
+      this.signupForm.controls.email.value,
+      this.signupForm.controls.username.value,
+      this.signupForm.controls.password.value
+    );
+    this.authService.signUp(user).subscribe(() => {
+      this.router.navigateByUrl('/auth/login');
+    }, error => this.errorSignup = true);
   }
 
 }
